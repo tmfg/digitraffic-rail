9b02ef0926ec39927691f8c6b3ec85ef6876ee3b
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2018-11-08 10:28:20 +0200

    DPO-194 interface implementation

--- LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/domain/entities/infra/Aikataulukausi.java
+++ LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/domain/entities/infra/Aikataulukausi.java
@@ -1,10 +1,12 @@
 package fi.livi.rata.avoindata.LiikeInterface.domain.entities.infra;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import fi.livi.rata.avoindata.LiikeInterface.domain.BaseEntity;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class Aikataulukausi extends BaseEntity {
@@ -13,12 +15,27 @@ public class Aikataulukausi extends BaseEntity {
     @Id
     @Column(name = KEY_NAME)
     private Long id;
+
     @Type(type="org.hibernate.type.LocalDateType")
     public LocalDate voimassaAlkuPvm;
+
     @Type(type="org.hibernate.type.LocalDateType")
     public LocalDate voimassaLoppuPvm;
 
-    @ManyToOne
+    @Type(type = "org.hibernate.type.LocalDateType")
+    @Column(name = "HAKU_LOPPU_PVM")
+    public LocalDate hakuLoppupvm;
+
+    @Type(type = "org.hibernate.type.LocalDateType")
+    public LocalDate jakopaatosViimeistaanPvm;
+
+    @OneToMany(mappedBy = "aikataulukausi")
+    public List<Muutosajankohta> muutosajankohdat;
+
+    public String nimi;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = Infra.KEY_NAME)
+    @JsonIgnore
     public Infra infra;
 }
