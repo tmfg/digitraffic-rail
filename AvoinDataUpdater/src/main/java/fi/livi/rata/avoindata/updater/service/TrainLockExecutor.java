725146d227bbae33e32a16cac4e4e66cfa1cda06
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-03-26 15:18:32 +0200

    Revert "Revert "PPP: fixed a train lock bug and wrote tests for it""
    This reverts commit 5ed4f30a

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/TrainLockExecutor.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/TrainLockExecutor.java
@@ -2,39 +2,40 @@ package fi.livi.rata.avoindata.updater.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Service
 public class TrainLockExecutor {
     private Logger log = LoggerFactory.getLogger(TrainLockExecutor.class);
 
+    final ExecutorService executorService = Executors.newSingleThreadExecutor();
+
     public <T> T executeInLock(Callable<T> callable) {
         ZonedDateTime submittedAt = ZonedDateTime.now();
 
-        try {
-            Pair<ZonedDateTime, T> returnPair = limitCall(callable);
-            ZonedDateTime executionStartedAt = returnPair.getFirst();
-
+        Callable<T> wrappedCallable = () -> {
+            ZonedDateTime executionStartedAt = ZonedDateTime.now();
+            T returnValue = callable.call();
             if (Duration.between(submittedAt, executionStartedAt).toMillis() > 10000) {
-                log.info("Waited: {}, Executed: {}",
-                        Duration.between(submittedAt, executionStartedAt),
-                        Duration.between(executionStartedAt, ZonedDateTime.now()));
+                log.info("Waited: {}, Executed: {}", Duration.between(submittedAt, executionStartedAt), Duration.between(executionStartedAt, ZonedDateTime.now()));
             }
-            return returnPair.getSecond();
+            return returnValue;
+        };
+
+        final Future<T> future = executorService.submit(wrappedCallable);
+
+        try {
+            return future.get();
         } catch (Exception e) {
             log.error("Error executing callable in TrainLockExecutor", e);
             return null;
         }
-    }
-
-    private synchronized <T> Pair<ZonedDateTime, T> limitCall(Callable<T> callable) throws Exception {
-        ZonedDateTime executionStartedAt = ZonedDateTime.now();
-
-        return Pair.of(executionStartedAt, callable.call());
     }
 }
