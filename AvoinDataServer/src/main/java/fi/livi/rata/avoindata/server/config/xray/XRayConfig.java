c5067633a2132e2ef8174033f753a02854aa6736
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-04-26 08:30:15 +0300

    DPO-770 x-ray testing

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/xray/XRayConfig.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/xray/XRayConfig.java
@@ -3,9 +3,11 @@ package fi.livi.rata.avoindata.server.config.xray;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
 import com.amazonaws.xray.strategy.FixedSegmentNamingStrategy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
 
+@Configuration
 public class XRayConfig {
     @Bean
     public Filter TracingFilter() {
