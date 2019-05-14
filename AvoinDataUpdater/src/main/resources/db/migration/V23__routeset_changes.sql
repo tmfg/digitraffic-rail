3951cb06aa458c77c6e36db0fdff8ae3a2c7d009
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-05-14 12:44:15 +0300

    DPO-49 fixed dbmaintain

--- AvoinDataUpdater/src/main/resources/db/migration/V23__routeset_changes.sql
+++ AvoinDataUpdater/src/main/resources/db/migration/V23__routeset_changes.sql
@@ -4,8 +4,7 @@ ADD COLUMN `message_id` BIGINT(20) NOT NULL AFTER `virtual_departure_date`;
 ALTER TABLE `routeset`
 CHANGE COLUMN `route_type` `route_type` VARCHAR(1) NOT NULL ,
 CHANGE COLUMN `train_number` `train_number` VARCHAR(8) NOT NULL ,
-CHANGE COLUMN `client_system` `client_system` VARCHAR(8) NOT NULL ,
-CHANGE COLUMN `virtual_departure_date` `virtual_departure_date` DATE NOT NULL ;
+CHANGE COLUMN `client_system` `client_system` VARCHAR(8) NOT NULL ;
 
 ALTER TABLE `routesection`
 CHANGE COLUMN `section_id` `section_id` VARCHAR(30) NOT NULL ,
