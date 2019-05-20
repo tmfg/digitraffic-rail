072d451ffc3d5c928deb6f277d3a269dd2fe1ce7
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-05-20 09:17:30 +0300

    DPO-49 npe fix

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/TrainPublishingService.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/TrainPublishingService.java
@@ -25,20 +25,22 @@ public class TrainPublishingService {
 
     public void publish(final List<Train> updatedTrains) {
         try {
-            for (Train train : updatedTrains) {
-                String trainAsString = objectMapper.writerWithView(TrainJsonView.LiveTrains.class).writeValueAsString(train);
+            if (updatedTrains != null) {
+                for (Train train : updatedTrains) {
+                    String trainAsString = objectMapper.writerWithView(TrainJsonView.LiveTrains.class).writeValueAsString(train);
 
-                mqttPublishService.publishString(
-                        String.format("trains/%s/%s/%s/%s/%s/%s/%s/%s", train.id.departureDate, train.id.trainNumber,
-                                train.trainCategory, train.trainType, train.operator.operatorShortCode, train.commuterLineID,
-                                train.runningCurrently, train.timetableType), trainAsString);
+                    mqttPublishService.publishString(
+                            String.format("trains/%s/%s/%s/%s/%s/%s/%s/%s", train.id.departureDate, train.id.trainNumber,
+                                    train.trainCategory, train.trainType, train.operator.operatorShortCode, train.commuterLineID,
+                                    train.runningCurrently, train.timetableType), trainAsString);
 
-                Set<String> announcedStations = new HashSet<>();
-                for (TimeTableRow timeTableRow : train.timeTableRows) {
-                    String stationShortCode = timeTableRow.station.stationShortCode;
-                    if (!announcedStations.contains(stationShortCode)) {
-                        announcedStations.add(stationShortCode);
-                        mqttPublishService.publishString(String.format("trains-by-station/%s", stationShortCode), trainAsString);
+                    Set<String> announcedStations = new HashSet<>();
+                    for (TimeTableRow timeTableRow : train.timeTableRows) {
+                        String stationShortCode = timeTableRow.station.stationShortCode;
+                        if (!announcedStations.contains(stationShortCode)) {
+                            announcedStations.add(stationShortCode);
+                            mqttPublishService.publishString(String.format("trains-by-station/%s", stationShortCode), trainAsString);
+                        }
                     }
                 }
             }
