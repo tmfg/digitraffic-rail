734342c8e87981fccabc2e699d3773361f424e1e
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-05-22 08:25:55 +0300

    PPP: set dns cache ttl to 10 because of RDS load balancing

---
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/DNSConfig.java
@@ -0,0 +1,13 @@
+package fi.livi.rata.avoindata.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class DNSConfig {
+    @PostConstruct
+    public void setTTL() {
+        java.security.Security.setProperty("networkaddress.cache.ttl", "10");
+    }
+}
