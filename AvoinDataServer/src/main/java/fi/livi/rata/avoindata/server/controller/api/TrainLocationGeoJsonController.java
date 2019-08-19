e8d883b5f34cf3841f53bbe64af3d47144451493
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-08-19 09:54:04 +0300

    DPO-839 AsGeoJson to method name so names are unique in graphql

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/controller/api/TrainLocationGeoJsonController.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/controller/api/TrainLocationGeoJsonController.java
@@ -33,14 +33,14 @@ public class TrainLocationGeoJsonController extends ADataController {
 
     @ApiOperation("Returns latest wsg84 coordinates for trains in geojson format")
     @RequestMapping(method = RequestMethod.GET, path = "latest")
-    public GeoJsonResponse getTrainLocations(@RequestParam(required = false) @ApiParam(example = "1,1,70,70") List<Double> bbox, HttpServletResponse response) {
+    public GeoJsonResponse getTrainLocationsAsGeoJson(@RequestParam(required = false) @ApiParam(example = "1,1,70,70") List<Double> bbox, HttpServletResponse response) {
         List<TrainLocation> trainLocations = trainLocationController.getTrainLocations(bbox, response);
         return geoJsonFormatter.wrapAsGeoJson(response, trainLocations, converter);
     }
 
     @ApiOperation("Returns latest wsg84 coordinates for a train in geojson format")
     @RequestMapping(method = RequestMethod.GET, path = "latest/{train_number}")
-    public GeoJsonResponse getTrainLocationByTrainNumber(@PathVariable @ApiParam(example = "1") Long train_number, @RequestParam(required = false) @ApiParam(example =
+    public GeoJsonResponse getTrainLocationByTrainNumberAsGeoJson(@PathVariable @ApiParam(example = "1") Long train_number, @RequestParam(required = false) @ApiParam(example =
             "1,1,70,70") List<Double> bbox, HttpServletResponse response) {
         List<TrainLocation> trainLocations = trainLocationController.getTrainLocationByTrainNumber(train_number, bbox, response);
         return geoJsonFormatter.wrapAsGeoJson(response, trainLocations, converter);
@@ -48,7 +48,7 @@ public class TrainLocationGeoJsonController extends ADataController {
 
     @ApiOperation("Returns wsg84 coordinates for a train run on departure date in geojson format")
     @RequestMapping(method = RequestMethod.GET, path = "{departure_date}/{train_number}")
-    public GeoJsonResponse getTrainLocationByTrainNumberAndDepartureDate(@PathVariable @ApiParam(example = "1") Long train_number, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate departure_date, @RequestParam(required = false) @ApiParam(example =
+    public GeoJsonResponse getTrainLocationByTrainNumberAndDepartureDateAsGeoJson(@PathVariable @ApiParam(example = "1") Long train_number, @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate departure_date, @RequestParam(required = false) @ApiParam(example =
             "1,1,70,70") List<Double> bbox, HttpServletResponse response) {
         List<TrainLocation> trainLocations = trainLocationController.getTrainLocationByTrainNumberAndDepartureDate(train_number, departure_date, bbox, response);
         return geoJsonFormatter.wrapAsGeoJson(response, trainLocations, converter);
