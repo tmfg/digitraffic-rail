10042b79885b336ac7135330d467e8a11322f194
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-11-13 09:03:33 +0200

    DPO-955 print defaultTransactionIsolation

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/DebugContextRefreshListener.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/DebugContextRefreshListener.java
@@ -1,5 +1,10 @@
 package fi.livi.rata.avoindata.server.config;
 
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +14,11 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
-import java.time.ZonedDateTime;
-import java.util.Arrays;
-import java.util.List;
+import com.google.common.base.Joiner;
 
 @Configuration
 public class DebugContextRefreshListener {
-    private List<String> printedProperties = Arrays.asList("spring.datasource.url", "spring.profiles.active","spring.datasource.driverClassName");
+    private List<String> printedProperties = Arrays.asList("spring.datasource.url", "spring.profiles.active", "spring.datasource.driverClassName", "spring.datasource.defaultTransactionIsolation");
     private Logger log = LoggerFactory.getLogger(DebugContextRefreshListener.class);
 
     @Autowired
@@ -24,10 +27,7 @@ public class DebugContextRefreshListener {
     @EventListener(ContextRefreshedEvent.class)
     private void printPropertyValues() {
         final Environment environment = applicationContext.getEnvironment();
-        for (final String printedProperty : printedProperties) {
-            log.info("Property {} = {}", printedProperty, environment.getProperty(printedProperty));
-        }
-
+        log.info("Properties: {}", Joiner.on(",").join(printedProperties.stream().map(s -> String.format("%s = %s", s, environment.getProperty(s))).collect(Collectors.toList())));
         log.info("It is now {}", ZonedDateTime.now());
     }
 }
