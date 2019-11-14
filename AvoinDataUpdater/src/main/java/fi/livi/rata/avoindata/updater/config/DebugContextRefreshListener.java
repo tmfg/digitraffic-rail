da32f7730b88729496bca07f96e8bf23c701a815
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-11-14 08:33:41 +0200

    DPO-955 dummy change

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/config/DebugContextRefreshListener.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/config/DebugContextRefreshListener.java
@@ -1,33 +1,31 @@
 package fi.livi.rata.avoindata.updater.config;
 
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
-import java.time.ZonedDateTime;
-import java.util.Arrays;
-import java.util.List;
+import com.google.common.base.Joiner;
 
 @Configuration(value = "DebugContextRefreshListener_AvoindataUpdater")
 public class DebugContextRefreshListener {
-    private List<String> printedProperties = Arrays.asList("spring.datasource.url", "spring.profiles.active", "updater.liikeinterface-url", "updater.mqtt.server-url");
+    private List<String> printedProperties = Arrays.asList("spring.datasource.url", "spring.profiles.active", "updater.liikeinterface-url", "updater.mqtt.server-url", "spring.datasource.driverClassName");
     private Logger log = LoggerFactory.getLogger(DebugContextRefreshListener.class);
 
     @Autowired
-    private ApplicationContext applicationContext;
+    Environment environment;
 
     @EventListener(ContextRefreshedEvent.class)
     private void printPropertyValues() {
-        final Environment environment = applicationContext.getEnvironment();
-        for (final String printedProperty : printedProperties) {
-            log.info("Property {} = {}", printedProperty, environment.getProperty(printedProperty));
-        }
-
+        log.info("Properties: {}", Joiner.on(",").join(printedProperties.stream().map(s -> String.format("%s = %s", s, environment.getProperty(s))).collect(Collectors.toList())));
         log.info("It is now {}", ZonedDateTime.now());
     }
 }
