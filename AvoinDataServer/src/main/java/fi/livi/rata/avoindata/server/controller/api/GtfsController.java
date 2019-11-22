456c84596b4c099af7d0d543e21de05d2d0d37ff
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-11-22 13:25:21 +0200

    DPO-970 add header for nodeping

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/controller/api/GtfsController.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/controller/api/GtfsController.java
@@ -1,15 +1,19 @@
 package fi.livi.rata.avoindata.server.controller.api;
 
-import fi.livi.rata.avoindata.common.dao.gtfs.GTFSRepository;
-import fi.livi.rata.avoindata.server.config.WebConfig;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "trains", description = "Returns trains", position = Integer.MIN_VALUE)
+import fi.livi.rata.avoindata.common.dao.gtfs.GTFSRepository;
+import fi.livi.rata.avoindata.common.domain.gtfs.GTFS;
+import fi.livi.rata.avoindata.server.config.WebConfig;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+@Api(tags = "trains", description = "Returns trains as gtfs", position = Integer.MIN_VALUE)
 @RestController
 @RequestMapping(WebConfig.CONTEXT_PATH + "trains")
 public class GtfsController {
@@ -19,13 +23,17 @@ public class GtfsController {
 
     @ApiOperation("Returns GTFS zip file")
     @RequestMapping(method = RequestMethod.GET, path = "gtfs-all.zip")
-    public byte[] getGtfsForAllTrains() {
-        return gtfsRepository.findFirstByFileNameOrderByIdDesc("gtfs-all.zip").data;
+    public byte[] getGtfsForAllTrains(HttpServletResponse response) {
+        GTFS gtfs = gtfsRepository.findFirstByFileNameOrderByIdDesc("gtfs-all.zip");
+        response.addHeader("GTFS-timestamp", gtfs.created.toString());
+        return gtfs.data;
     }
 
     @ApiOperation("Returns GTFS zip file")
     @RequestMapping(method = RequestMethod.GET, path = "gtfs-passenger.zip")
-    public byte[] getGtfsForPassengerTrains() {
-        return gtfsRepository.findFirstByFileNameOrderByIdDesc("gtfs-passenger.zip").data;
+    public byte[] getGtfsForPassengerTrains(HttpServletResponse response) {
+        GTFS gtfs = gtfsRepository.findFirstByFileNameOrderByIdDesc("gtfs-passenger.zip");
+        response.addHeader("GTFS-timestamp", gtfs.created.toString());
+        return gtfs.data;
     }
 }
