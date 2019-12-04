51054810bab15e7166e478ee9bded0e2683e5cc1
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-12-04 08:53:53 +0200

    PPP: configure timeouts properly

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/config/RestTemplateFactory.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/config/RestTemplateFactory.java
@@ -1,16 +1,19 @@
 package fi.livi.rata.avoindata.updater.config;
 
 
+import java.util.Arrays;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
-import java.util.Arrays;
 
 @Configuration
 class RestTemplateFactory {
@@ -25,11 +28,29 @@ class RestTemplateFactory {
     private int CONNECTION_TIMEOUT;
 
     @Bean
-    public RestTemplate createRestTemplate(RestTemplateBuilder restTemplateBuilder) {
-        restTemplateBuilder.messageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter[]{messageConverter}));
-        restTemplateBuilder.setConnectTimeout(Duration.ofMillis(CONNECTION_TIMEOUT));
-        restTemplateBuilder.setReadTimeout(Duration.ofMillis(READ_TIMEOUT));
+    public RequestConfig requestConfig() {
+        RequestConfig result = RequestConfig.custom()
+                .setConnectTimeout(CONNECTION_TIMEOUT)
+                .setSocketTimeout(READ_TIMEOUT)
+                .build();
+        return result;
+    }
 
-        return restTemplateBuilder.build();
+    @Bean
+    public CloseableHttpClient httpClient(RequestConfig requestConfig) {
+        CloseableHttpClient result = HttpClientBuilder
+                .create()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+        return result;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(HttpClient httpClient) {
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+        restTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter[]{messageConverter}));
+        return restTemplate;
     }
 }
