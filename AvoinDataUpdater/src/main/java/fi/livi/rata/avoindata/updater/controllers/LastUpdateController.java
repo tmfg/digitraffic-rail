4ce899e536f77afe65fccf1caae06b3e6b8bd7e3
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2019-12-23 13:26:01 +0200

    PPP: cache response for 1s

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/controllers/LastUpdateController.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/controllers/LastUpdateController.java
@@ -2,6 +2,8 @@ package fi.livi.rata.avoindata.updater.controllers;
 
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +19,9 @@ public class LastUpdateController {
 
     @RequestMapping("/last-updated")
     @ResponseBody
-    public Map<LastUpdateService.LastUpdatedType, IsUpToDateService.IsToUpToDateDto> getResponse() {
+    public Map<LastUpdateService.LastUpdatedType, IsUpToDateService.IsToUpToDateDto> getResponse(HttpServletResponse response) {
+        response.setHeader("Cache-Control", String.format("max-age=%d, public", 1));
+
         return isUpToDateService.getIsUpToDates();
     }
 }
