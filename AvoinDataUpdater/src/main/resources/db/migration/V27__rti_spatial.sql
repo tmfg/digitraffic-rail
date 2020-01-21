c3017a7f221bd07e0024ca70cc259825a777eff7
Massimo <massimo.prencipe@solita.fi>
2020-01-21 16:09:45 +0200

    DPO-1009 modify not null constraints to work with Aurora MySQL

--- AvoinDataUpdater/src/main/resources/db/migration/V27__rti_spatial.sql
+++ AvoinDataUpdater/src/main/resources/db/migration/V27__rti_spatial.sql
@@ -1,9 +1,12 @@
 DELETE FROM track_work_notification;
 
 ALTER TABLE track_work_notification
-    ADD location_map GEOMETRY NOT NULL,
-    ADD location_schema GEOMETRY NOT NULL
+    ADD location_map GEOMETRY,
+    ADD location_schema GEOMETRY
 ;
+ALTER TABLE track_work_notification
+    CHANGE COLUMN location_map location_map GEOMETRY NOT NULL,
+    CHANGE COLUMN location_schema location_schema GEOMETRY NOT NULL;
 
 ALTER TABLE ruma_location
     ADD location_map GEOMETRY,
@@ -11,6 +14,9 @@ ALTER TABLE ruma_location
 ;
 
 ALTER TABLE identifier_range
-    ADD location_map GEOMETRY NOT NULL,
-    ADD location_schema GEOMETRY NOT NULL
+    ADD location_map GEOMETRY,
+    ADD location_schema GEOMETRY
 ;
+ALTER TABLE identifier_range
+    CHANGE COLUMN location_map location_map GEOMETRY NOT NULL,
+    CHANGE COLUMN location_schema location_schema GEOMETRY NOT NULL;
