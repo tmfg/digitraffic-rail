88bc930c4944a60f6d35c3005f562a7a1f53a499
Massimo <massimo.prencipe@solita.fi>
2020-02-21 16:44:51 +0200

    DPO-897 fix import

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/updaters/TrackWorkNotificationUpdater.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/updaters/TrackWorkNotificationUpdater.java
@@ -13,6 +13,7 @@ import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
