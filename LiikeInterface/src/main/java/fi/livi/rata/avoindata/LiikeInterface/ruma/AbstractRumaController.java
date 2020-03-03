6a0e244da1083041b50141b1d4203846177a841b
Massimo Prencipe <massimo.prencipe@solita.fi>
2020-03-03 13:46:14 +0200

    DPO-897 close connection after use

--- LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/ruma/AbstractRumaController.java
+++ LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/ruma/AbstractRumaController.java
@@ -1,21 +1,30 @@
 package fi.livi.rata.avoindata.LiikeInterface.ruma;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class AbstractRumaController {
 
     protected String getFromRumaWithToken(String url, String token) throws IOException {
         URL u = new URL(url);
         HttpURLConnection conn = (HttpURLConnection) u.openConnection();
-        conn.setDoOutput(true);
-        conn.setRequestMethod("GET");
-        conn.setRequestProperty("Authorization", String.format("Bearer %s", token));
-        conn.setRequestProperty("Content-Type", "application/json");
-
-        return new String(conn.getInputStream().readAllBytes(), Charset.forName("utf8"));
+        InputStream is = null;
+        try {
+            conn.setDoOutput(true);
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Authorization", String.format("Bearer %s", token));
+            conn.setRequestProperty("Content-Type", "application/json");
+            is = conn.getInputStream();
+            return new String(is.readAllBytes(), StandardCharsets.UTF_8);
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+            conn.disconnect();
+        }
     }
 
 }
