56650d213f927fc493442d79b84e47b4384e2fbc
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2020-11-23 13:06:53 +0200

    Do not transfer kupla-loki information as it is to AWS for security reasons

--- LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/kuplat/KuplaController.java
+++ LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/kuplat/KuplaController.java
@@ -3,6 +3,9 @@ package fi.livi.rata.avoindata.LiikeInterface.kuplat;
 import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,12 +18,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import fi.livi.rata.avoindata.LiikeInterface.domain.JunapaivaPrimaryKey;
 import fi.livi.rata.avoindata.LiikeInterface.services.ClassifiedTrainFilter;
 
 @Controller
 public class KuplaController {
+    private class SijaintiDto {
+        public final Integer longitude;
+        public final Integer latitude;
+
+        public SijaintiDto(Integer longitude, Integer latitude) {
+            this.longitude = longitude;
+            this.latitude = latitude;
+        }
+    }
+
+    private class KuplaDto {
+        public final Integer junanumero;
+        public final LocalDate lahtopaiva;
+        public final Integer nopeus;
+        public final ZonedDateTime aikaleima;
+        public final SijaintiDto sijainti;
+
+        public KuplaDto(Integer junanumero, LocalDate lahtopaiva, Integer nopeus, ZonedDateTime aikaleima, SijaintiDto sijainti) {
+            this.junanumero = junanumero;
+            this.lahtopaiva = lahtopaiva;
+            this.nopeus = nopeus;
+            this.aikaleima = aikaleima;
+            this.sijainti = sijainti;
+        }
+    }
+
     private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
@@ -37,16 +65,12 @@ public class KuplaController {
 
     @RequestMapping(value = "/avoin/kuplas")
     @ResponseBody
-    public Iterable<JsonNode> getByVersion() throws IOException {
+    public Iterable<KuplaDto> getByVersion() throws IOException {
         final JsonNode kuplat = objectMapper.readTree(new URL(liikeBaseUrl + kuplaUrl));
 
         final JsonNode kuplatNodes = kuplat.get("kuplat");
 
-        final Iterable<JsonNode> filteredJunapaivaPrimaryKeys = classifiedTrainFilter.filterClassifiedTrains(
-                Lists.newArrayList(kuplatNodes.elements()),
-                n -> new JunapaivaPrimaryKey(n.get("junanumero").asText(), LocalDate.parse(n.get("lahtopaiva").asText())));
-
-        Iterable<JsonNode> filteredNullLocations = Iterables.filter(filteredJunapaivaPrimaryKeys, s -> {
+        Iterable<JsonNode> filteredNullLocations = Iterables.filter(kuplatNodes, s -> {
             if (s.get("sijainti") != null && s.get("sijainti").get("latitude") != null) {
                 return true;
             } else {
@@ -55,6 +79,21 @@ public class KuplaController {
             }
         });
 
-        return Lists.newArrayList(filteredNullLocations);
+        List<KuplaDto> kuplaDtoList = new ArrayList<>();
+        for (JsonNode kuplatNode : filteredNullLocations) {
+            SijaintiDto sijaintiDto = new SijaintiDto(kuplatNode.get("sijainti").get("longitude").asInt(), kuplatNode.get("sijainti").get("latitude").asInt());
+
+            KuplaDto kuplaDto = new KuplaDto(
+                    kuplatNode.get("junanumero").asInt(),
+                    LocalDate.parse(kuplatNode.get("lahtopaiva").asText()),
+                    kuplatNode.get("nopeus").asInt(),
+                    ZonedDateTime.parse(kuplatNode.get("aikaleima").asText()),
+                    sijaintiDto);
+            kuplaDtoList.add(kuplaDto);
+        }
+
+        return classifiedTrainFilter.filterClassifiedTrains(
+                kuplaDtoList,
+                n -> new JunapaivaPrimaryKey(n.junanumero.toString(), n.lahtopaiva));
     }
 }
