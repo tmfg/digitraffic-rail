af3565600284ebefc93c1b64b26d262f36d71d2d
Massimo Prencipe <massimo.prencipe@solita.fi>
2021-02-19 09:20:12 +0200

    DPO-1305 Update Swagger description

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/SwaggerConfig.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/SwaggerConfig.java
@@ -25,10 +25,19 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("rata.digitraffic.fi")
-                .description("API for trains operating in Finland")
-                .contact(new Contact("Google Groups", "http://groups.google.com/forum/#!forum/rata_digitraffic_fi", ""))
-                .license("Creative Commons Nimeä 4.0")
-                .licenseUrl("http://creativecommons.org/licenses/by/4.0/")
+                .description(
+                        "Digitraffic is a service operated by the [Fintraffic](https://www.fintraffic.fi) offering real time traffic information. \n" +
+                                "Currently the service covers *road, marine and rail* traffic. More information can be found at the [Digitraffic website](https://www.digitraffic.fi/) \n\n\n" +
+                                "The service has a public Google-group [road.digitraffic.fi](https://groups.google.com/forum/#!forum/roaddigitrafficfi) for \n" +
+                                "communication between developers, service administrators and Fintraffic. \n" +
+                                "The discussion in the forum is mostly in Finnish, but you're welcome to communicate in English too. \n\n\n" +
+                                "### General notes of the API\n\n" +
+                                "* Many Digitraffic APIs use GeoJSON as data format. Definition of the GeoJSON format can be found at https://tools.ietf.org/html/rfc7946.\n\n" +
+                                "* For dates and times [ISO 8601](https://en.wikipedia.org/wiki/ISO_8601) format is used with \"Zulu\" zero offset from UTC unless otherwise specified \n" +
+                                "(i.e., \"yyyy-mm-ddThh:mm:ss[.mmm]Z\"). E.g. 2019-11-01T06:30:00Z.")
+                .contact(new Contact("Digitraffic / Fintraffic", "https://www.digitraffic.fi/", ""))
+                .license("Digitraffic is an open data service. All content from the service and the service documentation is licenced under the Creative Commons 4.0 BY license.")
+                .licenseUrl("https://creativecommons.org/licenses/by/4.0/")
                 .version("1.0")
                 .build();
     }
