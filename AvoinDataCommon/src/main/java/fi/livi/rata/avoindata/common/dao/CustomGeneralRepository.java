0a28c0fb88c2d4922a1b13131f91c5d35f05c517
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2021-08-31 08:33:59 +0300

    Disable x-ray because it doesn't work

--- AvoinDataCommon/src/main/java/fi/livi/rata/avoindata/common/dao/CustomGeneralRepository.java
+++ AvoinDataCommon/src/main/java/fi/livi/rata/avoindata/common/dao/CustomGeneralRepository.java
@@ -7,10 +7,7 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import com.amazonaws.xray.spring.aop.XRayEnabled;
-
 @NoRepositoryBean
-@XRayEnabled
 public interface CustomGeneralRepository<T, ID extends Serializable>
         extends JpaRepository<T, ID> {
 
