1f41c3c24f6ae00e66dc606cea89a6b938e3f74d
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2021-12-02 08:30:34 +0200

    Removed unnecessary code

--- LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/metadata/repository/JunatyyppiRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.livi.rata.avoindata.LiikeInterface.metadata.repository;
-
-import fi.livi.rata.avoindata.LiikeInterface.domain.entities.Junatyyppi;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JunatyyppiRepository extends JpaRepository<Junatyyppi, Long> {
-}
--- LiikeInterface/src/main/java/fi/livi/rata/avoindata/LiikeInterface/metadata/repository/OperaattoriRepository.java
+++
@@ -1,9 +0,0 @@
-package fi.livi.rata.avoindata.LiikeInterface.metadata.repository;
-
-import fi.livi.rata.avoindata.LiikeInterface.domain.entities.Operaattori;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OperaattoriRepository extends JpaRepository<Operaattori, Long> {
-}
