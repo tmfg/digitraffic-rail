ea9723b72634976a771cfaf7a5855181e27f1704
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2021-12-02 11:00:22 +0200

    SB 2.1 -> 2.2

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/updaters/TrackWorkNotificationUpdater.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/updaters/TrackWorkNotificationUpdater.java
@@ -16,6 +16,7 @@ import org.locationtech.jts.geom.GeometryCollection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
