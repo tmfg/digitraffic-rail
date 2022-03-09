d1ebb51cdd5f8cab2b665fc506eedad82f882567
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2022-03-09 11:15:46 +0200

    GTFS: use extended route types

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/gtfs/GTFSTrainTypeService.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/gtfs/GTFSTrainTypeService.java
@@ -7,11 +7,19 @@ import fi.livi.rata.avoindata.updater.service.timetable.entities.Schedule;
 @Service
 public class GTFSTrainTypeService {
 
-    public static final int OTHER_TYPE = 1700;
+    public static final int OTHER_TYPE = 100;
     public static final int COMMUTER_TYPE = 109;
     public static final int LONG_DISTANCE_TYPE = 102;
 
     public int getGtfsTrainType(Schedule schedule) {
-        return 2;
+        if (!schedule.trainType.commercial) {
+            return OTHER_TYPE;
+        } else if (schedule.trainCategory.name.equals("Commuter")) {
+            return COMMUTER_TYPE;
+        } else if (schedule.trainCategory.name.equals("Long-distance")) {
+            return LONG_DISTANCE_TYPE;
+        } else {
+            return OTHER_TYPE;
+        }
     }
 }
