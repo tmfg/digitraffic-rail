9f3536e67d662ea7c2e9242e83ee3a98d5543d0d
Jaakko Paavilainen <jaakko.paavilainen@solita.fi>
2022-07-21 11:38:11 +0300

    Overwrite RU station locations because they are incorrect in source data

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/StationService.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/service/StationService.java
@@ -2,10 +2,12 @@ package fi.livi.rata.avoindata.updater.service;
 
 import fi.livi.rata.avoindata.common.dao.metadata.StationRepository;
 import fi.livi.rata.avoindata.common.domain.metadata.Station;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 
 @Service
@@ -15,6 +17,19 @@ public class StationService {
 
     @Transactional
     public void update(final Station[] stations) {
+        for (Station station : stations) {
+            if (station.shortCode.equals("PTR")) {
+                station.latitude = BigDecimal.valueOf(59.957131);
+                station.longitude = BigDecimal.valueOf(30.356308);
+            } else if (station.shortCode.equals("PTL")) {
+                station.latitude = BigDecimal.valueOf(59.9311424);
+                station.longitude = BigDecimal.valueOf(30.443925);
+            } else if (station.shortCode.equals("MVA")) {
+                station.latitude = BigDecimal.valueOf(55.777111);
+                station.longitude = BigDecimal.valueOf(37.655278);
+            }
+        }
+
         stationRepository.deleteAllInBatch();
         stationRepository.persist(Arrays.asList(stations));
     }
