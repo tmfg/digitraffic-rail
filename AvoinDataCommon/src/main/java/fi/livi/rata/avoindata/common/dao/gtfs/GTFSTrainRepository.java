c19c65838e3cc87ca8f3bf3494a23d70e5d89813
Teijo Rönkä <teijo.ronka@solita.fi>
2022-08-19 14:38:58 +0300

    DPO-1313 revert optimization

--- AvoinDataCommon/src/main/java/fi/livi/rata/avoindata/common/dao/gtfs/GTFSTrainRepository.java
+++ AvoinDataCommon/src/main/java/fi/livi/rata/avoindata/common/dao/gtfs/GTFSTrainRepository.java
@@ -13,9 +13,7 @@ import java.util.List;
 @Repository
 public interface GTFSTrainRepository extends CustomGeneralRepository<GTFSTrain, TrainId> {
     @Query("select train from GTFSTrain train" +
-            " inner join train.timeTableRows as row" +
             " where train.version > ?1" +
-            " and train.id.departureDate between (current_date - 1) and current_date" +
-            " and row.id.departureDate between (current_date - 1) and current_date")
+            " and train.id.departureDate between (current_date - 1) and current_date")
     List<GTFSTrain> findByVersionGreaterThan(final long version);
 }
