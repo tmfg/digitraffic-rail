b8d9b141a1a74a5f1f74ea2c1b2d4356396f0a18
Ilari Junnonen <ilari.junnonen@solita.fi>
2022-09-01 13:53:02 +0300

    DPO-1822 Add HTTP POST endpoint for RAMI messages

---
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/config/ApiKeyValidationFilter.java
@@ -0,0 +1,67 @@
+package fi.livi.rata.avoindata.updater.config;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+@Component
+public class ApiKeyValidationFilter extends GenericFilterBean {
+
+    @Value("${rami.api-key}")
+    String ramiApiKey;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain) {
+
+        final HttpServletRequest req = (HttpServletRequest) request;
+
+        final String path = req.getRequestURI();
+
+        if (!path.startsWith("/rami")) {
+            try {
+                chain.doFilter(request, response);
+                return;
+            } catch (IOException | ServletException error) {
+                logger.error(error.getMessage());
+            }
+        }
+
+        final String apiKey = req.getHeader("API-KEY") == null ? "" : req.getHeader("API-KEY");
+
+        if (apiKey.equals(ramiApiKey)) {
+            try {
+                chain.doFilter(request, response);
+                return;
+            } catch (IOException | ServletException error) {
+                logger.error(error.getMessage());
+            }
+        } else {
+            final HttpServletResponse resp = (HttpServletResponse) response;
+            final String errorMessage = "Invalid API-KEY";
+
+            resp.reset();
+            resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            resp.setContentLength(errorMessage.length());
+
+            try {
+                resp.getWriter().write(errorMessage);
+            } catch (IOException error) {
+                logger.error(error.getMessage());
+            }
+        }
+    }
+}
+
---
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/controllers/RamiIntegrationController.java
@@ -0,0 +1,26 @@
+package fi.livi.rata.avoindata.updater.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@Controller
+public class RamiIntegrationController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @PostMapping("/rami/incoming")
+    @ResponseBody
+    public ResponseEntity handleIncoming(@RequestBody
+                                  JsonNode message) {
+        logger.info("Received RAMI message: {}", message);
+        return ResponseEntity.ok().build();
+    }
+
+}
