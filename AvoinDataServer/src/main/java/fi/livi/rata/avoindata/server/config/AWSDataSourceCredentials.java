75c29e5c4e5cbe002f2c0888e9db4f5a383cfb97
Joonas-M-S <joonas.mikkonen@solita.fi>
2023-02-22 15:05:16 +0200

    DPO-1492: Configure the datasource to use the credentials from Secret Manager if running on AWS

---
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/AWSDataSourceCredentials.java
@@ -0,0 +1,32 @@
+package fi.livi.rata.avoindata.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component("awsDataSourceCredentials")
+@Profile("aws")
+public class AWSDataSourceCredentials {
+    final private String username;
+    final private String password;
+    final private String url;
+
+    // Database credentials are fetched from the AWS Secrets Manager
+    AWSDataSourceCredentials(final @Value("${url}") String url,
+                             final @Value("${username}") String username,
+                             final @Value("${password}") String password) {
+        this.username = username;
+        this.password = password;
+        this.url = url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getUrl() {
+        return url;
+    }
+}
--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/RailApplicationConfiguration.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/RailApplicationConfiguration.java
@@ -2,20 +2,26 @@ package fi.livi.rata.avoindata.server.config;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class RailApplicationConfiguration {
-    // Database credentials are fetched from the AWS Secrets Manager
+
+    @Autowired
+    private Environment env;
+
     @Bean
     @Primary
-    public DataSource dataSource(final @Value("${url}") String url,
-                                 final @Value("${username}") String username,
-                                 final @Value("${password}") String password) {
+    public DataSource dataSource(@Autowired(required = false) AWSDataSourceCredentials awsDataSourceCredentials) {
+        String url = awsDataSourceCredentials != null ? awsDataSourceCredentials.getUrl() : env.getProperty("spring.datasource.url");
+        String username = awsDataSourceCredentials != null ? awsDataSourceCredentials.getUsername() : env.getProperty("spring.datasource.username");
+        String password = awsDataSourceCredentials != null ? awsDataSourceCredentials.getPassword() : env.getProperty("spring.datasource.password");
+
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create(); 
         dataSourceBuilder.url(url);
         dataSourceBuilder.username(username); 
