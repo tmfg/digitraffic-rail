9cc02b6c6b72c18e29edbd2c4d927f4bff53efc1
Ilari Junnonen <ilari.junnonen@solita.fi>
2023-06-01 13:14:59 +0300

    DPO-1822 Send rami messages to sqs

--- AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/controllers/RamiIntegrationController.java
+++ AvoinDataUpdater/src/main/java/fi/livi/rata/avoindata/updater/controllers/RamiIntegrationController.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,13 +13,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.networknt.schema.ValidationMessage;
 
 import fi.livi.rata.avoindata.updater.service.rami.RamiValidationService;
 
 @Controller
-@ConditionalOnProperty(prefix="rami", name="enabled", havingValue="true")
+@ConditionalOnProperty(prefix = "rami",
+                       name = "enabled",
+                       havingValue = "true")
 public class RamiIntegrationController {
 
     public static final String BASE_PATH = "/api/v1/rami/incoming";
@@ -28,11 +34,16 @@ public class RamiIntegrationController {
 
     private final RamiValidationService ramiValidationService;
 
-    public RamiIntegrationController(final RamiValidationService ramiValidationService) {
+    private final String queueUrl;
+
+    public RamiIntegrationController(final RamiValidationService ramiValidationService,
+                                     @Value("${rami.sqs.url}")
+                                     final String queueUrl) {
         this.ramiValidationService = ramiValidationService;
+        this.queueUrl = queueUrl;
     }
 
-    @PostMapping(value = { BASE_PATH + "/message",  BASE_PATH_ALTERNATE + "/message"},
+    @PostMapping(value = { BASE_PATH + "/message", BASE_PATH_ALTERNATE + "/message" },
                  consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ResponseEntity handleMessage(
@@ -41,11 +52,21 @@ public class RamiIntegrationController {
         final Set<ValidationMessage> errors = ramiValidationService.validateRamiMessage(body);
         if (errors.isEmpty()) {
             logger.info("Received valid RAMI message: {}", body);
+            sendToQueue(body);
             return ResponseEntity.ok().build();
         } else {
             logger.warn("Received invalid RAMI message: {} with validation errors: {}", body, errors);
             return ResponseEntity.badRequest().body(errors.toString());
         }
+    }
+
+    private void sendToQueue(final JsonNode ramiMessage) {
+        AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+        SendMessageRequest send_msg_request = new SendMessageRequest()
+                .withQueueUrl(queueUrl)
+                .withMessageBody(ramiMessage.toString())
+                .withDelaySeconds(5);
+        sqs.sendMessage(send_msg_request);
     }
 
 }
