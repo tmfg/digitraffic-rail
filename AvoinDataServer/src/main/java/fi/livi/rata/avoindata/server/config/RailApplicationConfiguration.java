45b8f1cb14d6ac4a20711a79a029aaaf63a671ad
Ilari Junnonen <92532596+solita-ijunnone@users.noreply.github.com>
2023-09-06 08:26:44 +0300

    DPO-2328 Set datasource properties in RailApplicationConfiguration(#39)

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/RailApplicationConfiguration.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/RailApplicationConfiguration.java
@@ -3,11 +3,13 @@ package fi.livi.rata.avoindata.server.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 public class RailApplicationConfiguration {
@@ -17,17 +19,46 @@ public class RailApplicationConfiguration {
 
     @Bean
     @Primary
-    public DataSource dataSource(@Autowired(required = false) final AWSDataSourceCredentials awsDataSourceCredentials) {
-        final String url = env.getProperty("spring.datasource.url");
-        final String driverClassName = env.getProperty("spring.datasource.driver-class-name");
-        final String username = awsDataSourceCredentials != null ? awsDataSourceCredentials.getUsername() : env.getProperty("spring.datasource.username");
-        final String password = awsDataSourceCredentials != null ? awsDataSourceCredentials.getPassword() : env.getProperty("spring.datasource.password");
+    public DataSource dataSource(
+            @Autowired(required = false)
+            final AWSDataSourceCredentials awsDataSourceCredentials,
+            @Value("${spring.datasource.url}")
+            final String url,
+            @Value("${spring.datasource.username}")
+            final String username,
+            @Value("${spring.datasource.password}")
+            final String password,
+            @Value("${spring.datasource.driver-class-name}")
+            final String driverClassName,
+            @Value("${spring.datasource.hikari.maximum-pool-size}")
+            final int maximumPoolSize,
+            @Value("${spring.datasource.hikari.max-lifetime}")
+            final int maxLifetime,
+            @Value("${spring.datasource.hikari.idle-timeout}")
+            final int idleTimeout,
+            @Value("${spring.datasource.hikari.connection-timeout}")
+            final int connectionTimeout,
+            @Value("${spring.datasource.hikari.leak-detection-threshold}")
+            final int leakDetectionThreshold,
+            @Value("${spring.datasource.hikari.read-only}")
+            final boolean readOnly
+    ) {
 
-        final DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.url(url);
-        dataSourceBuilder.username(username); 
-        dataSourceBuilder.password(password); 
-        dataSourceBuilder.driverClassName(driverClassName);
-        return dataSourceBuilder.build();
+        final String actualUsername = awsDataSourceCredentials != null ? awsDataSourceCredentials.getUsername() : username;
+        final String actualPassword = awsDataSourceCredentials != null ? awsDataSourceCredentials.getPassword() : password;
+
+        final HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(url);
+        dataSource.setUsername(actualUsername);
+        dataSource.setPassword(actualPassword);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setMaximumPoolSize(maximumPoolSize);
+        dataSource.setMaxLifetime(maxLifetime);
+        dataSource.setIdleTimeout(idleTimeout);
+        dataSource.setConnectionTimeout(connectionTimeout);
+        dataSource.setLeakDetectionThreshold(leakDetectionThreshold);
+        dataSource.setReadOnly(readOnly);
+
+        return dataSource;
     }
 }
