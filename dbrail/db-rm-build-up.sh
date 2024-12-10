29a1a3774d171472c8920c518d162084b9b2626d
jouniso <jouniso@users.noreply.github.com>
2024-12-10 15:17:13 +0100

    Universal db-rm-build-up.sh

--- dbrail/db-rm-build-up.sh
+++ dbrail/db-rm-build-up.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker compose rm && docker compose up
+if [ -x "$(command -v docker-compose)" ]; then
+  docker-compose down && docker-compose rm db && docker-compose build && docker-compose up
+else
+  docker compose down && docker compose rm db && docker compose build && docker compose up
+fi
