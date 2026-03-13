5dbdfacb5f8538c27524a3563dab3c22c95835fd
Joonas-M-S <joonas.mikkonen@solita.fi>
2026-03-13 16:37:29 +0200

    Update common

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/JacksonConfig.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/config/JacksonConfig.java
@@ -28,6 +28,7 @@ public class JacksonConfig {
                     .disable(DateTimeFeature.WRITE_DATES_AS_TIMESTAMPS)
                     .enable(DateTimeFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE)
                     .enable(DateTimeFeature.WRITE_DATES_WITH_CONTEXT_TIME_ZONE)
+                    .defaultTimeZone(TimeZone.getTimeZone("UTC"))
                     .addModule(new JtsModule())
                     .addModule(new StreamModule());
         };
