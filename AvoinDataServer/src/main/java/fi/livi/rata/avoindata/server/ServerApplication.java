ae78ca899d4e42e4462ee1992c0de08249ca837f
Joonas-M-S <joonas.mikkonen@solita.fi>
2026-03-16 14:56:18 +0200

    Update import

--- AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/ServerApplication.java
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/ServerApplication.java
@@ -3,7 +3,7 @@ package fi.livi.rata.avoindata.server;
 import fi.livi.rata.avoindata.common.dao.CustomGeneralRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
