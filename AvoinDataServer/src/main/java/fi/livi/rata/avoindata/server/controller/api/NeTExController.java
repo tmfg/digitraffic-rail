d3310f4d0074296bb1ddc055d07c43da3f17fab6
Ilari Junnonen <ilari.junnonen@solita.fi>
2026-06-30 16:36:57 +0300

    DPO-4755 serve static package

---
+++ AvoinDataServer/src/main/java/fi/livi/rata/avoindata/server/controller/api/NeTExController.java
@@ -0,0 +1,47 @@
+package fi.livi.rata.avoindata.server.controller.api;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fi.livi.rata.avoindata.common.dao.gtfs.GeneratedExportRepository;
+import fi.livi.rata.avoindata.common.domain.gtfs.GeneratedExport;
+import fi.livi.rata.avoindata.common.utils.DateProvider;
+import fi.livi.rata.avoindata.server.config.WebConfig;
+import fi.livi.rata.avoindata.server.controller.utils.CacheControl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Tag(name = "netex", description = "Returns trains in NeTEx Nordic format)")
+@RestController
+@RequestMapping(WebConfig.CONTEXT_PATH + "trains")
+public class NeTExController {
+
+    private static final String NETEX_FILENAME = "netex-nordic.zip";
+    private static final int CACHE_SECONDS = 60 * 15;
+
+    private final GeneratedExportRepository generatedExportRepository;
+
+    public NeTExController(final GeneratedExportRepository generatedExportRepository) {
+        this.generatedExportRepository = generatedExportRepository;
+    }
+
+    @Operation(summary = "Returns NeTEx Nordic static timetable ZIP")
+    @RequestMapping(method = RequestMethod.GET, path = "netex-nordic.zip", produces = "application/zip")
+    @Transactional(readOnly = true)
+    public byte[] getNeTExNordic(final HttpServletResponse response) {
+        CacheControl.setCacheMaxAgeSeconds(response, CACHE_SECONDS);
+
+        final GeneratedExport export = generatedExportRepository.findFirstByFileNameOrderByIdDesc(NETEX_FILENAME);
+
+        response.addHeader("x-is-fresh",
+                Boolean.toString(export.created.isAfter(DateProvider.nowInHelsinki().minusHours(25))));
+        response.addHeader("x-timestamp", export.created.toString());
+        response.addHeader(HttpHeaders.CONTENT_LENGTH, String.valueOf(export.data.length));
+
+        return export.data;
+    }
+}
